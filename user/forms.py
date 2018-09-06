894f2b262da935691e532f3ec49ce17e1ef198e7
Levent GUZELOGLU <gun.guzeloglu@gmail.com>
2018-09-06 11:46:45 +0300

    Register Form works.

--- user/forms.py
+++ user/forms.py
@@ -3,6 +3,8 @@ from wtforms import validators, StringField, PasswordField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import ValidationError
 
+from user.models import User
+
 
 class RegisterForm(Form):
     first_name = StringField('First Name', [validators.DataRequired()])
@@ -14,3 +16,11 @@ class RegisterForm(Form):
     password = PasswordField('New Password', [validators.DataRequired(), validators.EqualTo(
         'confirm', message='Passwords must match'), validators.length(min=4, max=80)])
     confirm = PasswordField('Repeat Password')
+
+    def validate_username(form, field):  # custom validator
+        if User.objects.filter(username=field.data).first():
+            raise ValidationError('Username already exists.')
+
+    def validate_email(form, field):
+        if User.objects.filter(email=field.data).first():
+            raise ValidationError("Email already in use.")
--- user/views.py
+++ user/views.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template
 from user.forms import RegisterForm
+import bcrypt
+from user.models import User
 
 user_app = Blueprint('user_app', __name__)  # naming the app
 
@@ -12,4 +14,14 @@ def login():
 @user_app.route('/register', methods=["GET", "POST"])
 def register():
     form = RegisterForm()
+    if form.validate_on_submit():
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(form.password.data, salt)
+        user = User(username=form.username.data,
+                    password=hashed_password,
+                    email=form.email.data,
+                    first_name=form.first_name.data,
+                    last_name=form.last_name.data)
+        user.save()
+        return "User registered"
     return render_template('user/register.html', form=form)
