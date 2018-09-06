1fd9725c9bf3bec4ec808ec963484a05e4035981
Levent GUZELOGLU <gun.guzeloglu@gmail.com>
2018-09-06 09:18:12 +0300

    mlab connection working and first record inserted.

--- application.py
+++ application.py
@@ -1,4 +1,9 @@
 from flask import Flask
+from flask_mongoengine import MongoEngine
+
+
+db = MongoEngine()
+
 
 # Factory
 
@@ -6,4 +11,9 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('settings.py')
+
+    from user.views import user_app
+    app.register_blueprint(user_app)
+    db.connect(db=app.config["MONGO_DBNAME"], username=app.config["MONGO_USERNAME"], password=app.config["MONGO_PASSWORD"], host=app.config["MONGO_URI"])
+    db.init_app(app)  # Initialize db
     return app
